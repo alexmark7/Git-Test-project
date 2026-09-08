87db5f4a6931158428d6c91b7ccd3d44a46d1983
alexmark7 <alexander.mankvard@gmail.com>
2026-09-08 17:47:59 +0200

    VSCODE konflikt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello from IntelliJ!");
+        System.out.println("Hello from VSCODE KONFLIKT!");
     }
 }
