392afa79c7224b72ee67c819166202843337e948
alexmark7 <alexander.mankvard@gmail.com>
2026-09-08 18:00:07 +0200

    Commit mac

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello from VSCODE KONFLIKT!");
+        System.out.println("Hello from VSCODE KONFLIKT hej hej hej dette er en test!");
     }
 }
